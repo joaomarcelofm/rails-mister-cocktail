2c883809804c48fb22c162da1d26d75f24c8e196
Joao Martins <joaomarcelofm@gmail.com>
2017-11-16 12:00:08 +0100

    model fixes

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
+  validates :description, :cocktail, :ingredient, presence: true
+
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
