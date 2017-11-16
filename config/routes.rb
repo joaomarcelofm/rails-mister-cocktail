5c165a4757d6af4abd37b0e29df5af843965e6c8
Joao Martins <joaomarcelofm@gmail.com>
2017-11-16 12:26:34 +0100

    Router fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   resources :doses, only: [:destroy]
 
+  root to: 'cocktails#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
