88990d766e289df0a7a1e5f31d0280460fc9c532
Joao Martins <joaomarcelofm@gmail.com>
2017-11-16 12:03:06 +0100

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
