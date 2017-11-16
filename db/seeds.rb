c727de70fc124a1133f6c4ac09c138b377114d28
Joao Martins <joaomarcelofm@gmail.com>
2017-11-16 17:22:09 +0100

    Seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Cocktail.create(name: "Test1")
-Ingredient.create(name: 'ITeste1')
-# Dose.create(description: "1cl", cocktail_id: 1, ingredient_id: 1)
 
-
-# Cocktail.create(name: "Test2")
-Ingredient.create(name: 'ITeste2')
-# Dose.create(description: "2cl", cocktail_id: 2, ingredient_id: 2)
-
-
-# Cocktail.create(name: "Test3")
-Ingredient.create(name: 'ITeste3')
-# Dose.create(description: "3cl", cocktail_id: 3, ingredient_id: 3)
-
-
-# Cocktail.create(name: "Test4")
-Ingredient.create(name: 'ITeste4')
-# Dose.create(description: "4cl", cocktail_id: 4, ingredient_id: 4)
+15.times do |ingredient|
+  Ingredient.create({
+    name: Faker::Food.spice
+    })
+  end
